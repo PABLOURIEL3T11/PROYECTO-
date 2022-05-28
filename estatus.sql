8f660846369485e0fa7b5362275e85f3b57fc6e9
PABLOURIEL3T11 <102884693+PABLOURIEL3T11@users.noreply.github.com>
2022-05-28 00:25:03 -0500

    Add files via upload

---
+++ estatus.sql
@@ -0,0 +1,25 @@
+create table estatus;
+use alumnos;
+create table estatus(
+id int primary key auto_increment,
+aceptado varchar(50),
+inscrito varchar(50),
+matricula int(50),
+carrera varchar(50),
+semestre varchar(50),
+foce int(50),
+numerodematerias int(50)
+);
+insert into estatus(inscrito,matricula,carrera,semestre,foce,numerodematerias) values('SI','SI','546466667','GASTRONOMIA','1','FOCE-897','6');
+insert into estatus(inscrito,matricula,carrera,semestre,foce,numerodematerias) values('SI','SI','5567767878','SISTEMAS','1','FOCE-876','6');
+insert into estatus(inscrito,matricula,carrera,semestre,foce,numerodematerias) values('SI','SI','5345665676','RENOVABLES','1','FOCE-567','6');
+insert into estatus(inscrito,matricula,carrera,semestre,foce,numerodematerias)values ('SI','SI','5568767878','SISTEMAS','1','FOCE-876','6');
+insert into estatus(inscrito,matricula,carrera,semestre,foce,numerodematerias) values('SI','SI','55565676776','TICS','1','FOCE-574','6');
+insert into estatus(inscrito,matricula,carrera,semestre,foce,numerodematerias) values('SI','SI','5467767878','SISTEMAS','1','FOCE-876','6');
+insert into estatus(inscrito,matricula,carrera,semestre,foce,numerodematerias) values('SI','SI','56665676677','ADMINISTRACION','1','FOCE-677','6');
+insert into estatus(inscrito,matricula,carrera,semestre,foce,numerodematerias) values('NO','NOINSCRITO','NOT','NOT','NOT','NOT','NONE');
+insert into estatus(inscrito,matricula,carrera,semestre,foce,numerodematerias) values('NO','NOINSCRITO','NOT','NOT','NOT','NOT','NONE');
+insert into estatus(inscrito,matricula,carrera,semestre,foce,numerodematerias) values('SI','SI','5565676776','ADMINISTRACCION','1','FOCE-677','6');
+insert into estatus(inscrito,matricula,carrera,semestre,foce,numerodematerias) values('SI','SI','6456655644','CONTABILIDAD','1','FOCE-768','6');
+insert into estatus(inscrito,matricula,carrera,semestre,foce,numerodematerias) values('SI','SI','55545345455','TICS','1','FOCE-574','6');
+insert into estatus(inscrito,matricula,carrera,semestre,foce,numerodematerias) values('NO','NOINSCRITO','NOT','NOT','NOT','NOT','NONE');
