c7ca2b10212b23b5f0068982d9eb2e806c40c5a1
PABLOURIEL3T11 <102884693+PABLOURIEL3T11@users.noreply.github.com>
2022-05-28 00:24:43 -0500

    Add files via upload

---
+++ documentos.sql
@@ -0,0 +1,24 @@
+create table Documento;
+use alumnos;
+create table Documento(
+id int primary key not null auto_increment,
+curp varchar(50),
+baucherdepago int(50),
+ine varchar(50),
+certificadodeprepa varchar(50),
+actadenacimiento varchar(50),
+actadeexamen varchar(50)
+);
+insert into documentos(curp,baucherdepago,ine,certificadodeprepa,actadenacimiento,actadeexamen) values ('ped345c234bre345','5667475899','pedrocabrera2345','8.6-anexadechalco','distritofederal','TESOEM-GASTRONOMIA');
+insert into documentos(curp,baucherdepago,ine,certificadodeprepa,actadenacimiento,actadeexamen) values ('carlos2345martinez345','66785959','carlosmartinez345','9.8-cecytem','estadodemexico','TESOEM-SISTEMASCOMPUTACIONALES');
+insert into documentos(curp,baucherdepago,ine,certificadodeprepa,actadenacimiento,actadeexamen) values ('andrea3455martinez446','75768689','andreamartinez4567','8.9-cbt35','distritofederal','TESOEM-RENOVABLES');
+insert into documentos(curp,baucherdepago,ine,certificadodeprepa,actadenacimiento,actadeexamen) values('jorge4566romero3456','456778887','jorgeromero23456986','7.6-cecytem','oaxaca','TESOEM-CECYTEM');
+insert into documentos(curp,baucherdepago,ine,certificadodeprepa,actadenacimiento,actadeexamen) values ('jose2345zavalartkjto45','4567678','josezavala85869679','8.9-cetis34','distritofederal','TESOEM-TICS');
+insert into documentos(curp,baucherdepago,ine,certificadodeprepa,actadenacimiento,actadeexamen) values ('angela2345645rodriguez','3534557','7.9-cbta35','distritofederal','TESOEM-SISTEMASCOMPURACIONALES');
+insert into documentos(curp,baucherdepago,ine,certificadodeprepa,actadenacimiento,actadeexamen) values ('jairzhino64758camacho','34566767','jairzhino34556676','7.5-cecytem','distritofederal','TESOEM-ADMINISTRACION');
+insert into documentos(curp,baucherdepago,ine,certificadodeprepa,actadenacimiento,actadeexamen) values ('alfonso5665coraona4luna','327698','alfonso344556','9.9-cobaem18','distritofederal','TESOEM-ADMINISTRACION');
+insert into documentos(curp,baucherdepago,ine,certificadodeprepa,actadenacimiento,actadeexamen) values ('pablo2345paz104','345567678','pablouriel938569','9.9-cecytem','distritofederal','TESOEM-GASTRONOMIA');
+insert into documentos(curp,baucherdepago,ine,certificadodeprepa,actadenacimiento,actadeexamen) values ('dulce2345maria34tkjto45','544567544','dulce45665maria23','8.9-cetis45','distritofederal','TESOEM-ADMINISTRACION');
+insert into documentos(curp,baucherdepago,ine,certificadodeprepa,actadenacimiento,actadeexamen) values ('carlos33455romero3452re','45556844','carlos234545romero12','7.3-bachilleres','estadodemexico','TESOEM-CONTABILIDAD');
+insert into documentos(curp,baucherdepago,ine,certificadodeprepa,actadenacimiento,actadeexamen) values ('fatimamichel2345zreyes455','55677667','fatimamichel234358','7.4-cecytem','estadodemexico','TESOEM-TICS');
+insert into documentos(curp,baucherdepago,ine,certificadodeprepa,actadenacimiento,actadeexamen) values ('mariazuñiga345434544ter','356675664','maria23443zuuñiga34','7.0-cobaem','estadodemexico','TESOEM-GASTRONOMIA');
