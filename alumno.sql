bb99fe3e812fa8535be9c799ec82ab9e260e4d35
PABLOURIEL3T11 <102884693+PABLOURIEL3T11@users.noreply.github.com>
2022-05-28 00:24:25 -0500

    Add files via upload

---
+++ alumno.sql
@@ -0,0 +1,36 @@
+
+use alumnos;
+create table alumno(
+id  int primary key not null auto_increment,
+nombre VARCHAR(50),
+    apat VARCHAR(50),
+    amat VARCHAR(50),
+    fechadenacimiento date,
+    sexo CHAR(1),
+    celular VARCHAR(50),
+    email VARCHAR(50),
+    localidad varchar(50),
+    contraseña VARCHAR(50),
+    cp int,
+    calle VARCHAR(50),
+    numeroint int,
+    numeroext int 
+    );
+    
+insert into alumno(nombre, apat, amat, fechadenacimiento, sexo, celular, email,localidad, contraseña, cp, calle, numeroint, numeroext) values('pedro', 'cabrera', 'salas', '1999-07-20', 'M', '5555555555', 'pedrocabrera123@correo.com', 'cuatrovientos ', '1234', '56530', 'arcoiris', '200', '23'); 
+insert into alumno(nombre, apat, amat, fechadenacimiento, sexo, celular, email,localidad, contraseña, cp, calle, numeroint, numeroext) values('carlos', 'martinez', 'oca', '2001-03-24', 'M', '55908499848', 'carlosoca123@gmail.com', 'cuatrovientos ', '1234', '56590', 'ventisca', '12', '44');
+insert into alumno(nombre, apat, amat, fechadenacimiento, sexo, celular, email,localidad, contraseña, cp, calle, numeroint, numeroext) values('andrea', 'martinez', 'hernandez', '2002-04-15', 'F', '559893494', 'andreaheriberto@hotmail.com', 'valledechalco ', '1234', '36778', 'jardines', '19', '23'); 
+insert into alumno(nombre, apat, amat, fechadenacimiento, sexo, celular, email,localidad, contraseña, cp, calle, numeroint, numeroext) values('jorge', 'romero', 'martinez', '2002-07-31', 'M', '55673845545', 'jorgemartinez@gmail.com', 'ixtapaluca ', '1234', '58944', 'municipio', '123', '10'); 
+insert into alumno(nombre, apat, amat, fechadenacimiento, sexo, celular, email,localidad, contraseña, cp, calle, numeroint, numeroext) values('jose', 'zavala', 'angeles', '1998-02-02', 'M', '555783849455', 'joseantonio@hotmail.com', 'lapaz', '1234', '56589', 'vias','134', '356');
+insert into alumno(nombre, apat, amat, fechadenacimiento, sexo, celular, email,localidad, contraseña, cp, calle, numeroint, numeroext) values('angela', 'rodriguez', 'salvador', '2001-03-19', 'F', '5557889383', 'ANGELASALVADOR@YAHOO.com', 'sanbuenaventura', '1234', '589383', 'cobaem','567', '35');
+insert into alumno(nombre, apat, amat, fechadenacimiento, sexo, celular, email,localidad, contraseña, cp, calle, numeroint, numeroext) values('jairzhino', 'camacho', 'romero', '2001-05-06', 'M', '555783889', 'huerojairzhi@gmail.com', 'valledechalco', '1234', '589985', 'solilaridad','13', '354');
+insert into alumno(nombre, apat, amat, fechadenacimiento, sexo, celular, email,localidad, contraseña, cp, calle, numeroint, numeroext) values('alfonso', 'luna', 'corona', '2001-10-21', 'M', '55390879', 'alfonsocorona@gmailcom', 'neza', '1234', '344556', 'metroneza','21', '67');
+insert into alumno(nombre, apat, amat, fechadenacimiento, sexo, celular, email,localidad, contraseña, cp, calle, numeroint, numeroext) values('pablouriel', 'paz', 'zuñiga', '2001-04-13', 'M', '5535094098', 'pabopazzuñiga@gmailcom', 'cuatrovientos', '1234', '56589', 'arcoiris','10', '34');
+insert into alumno(nombre, apat, amat, fechadenacimiento, sexo, celular, email,localidad, contraseña, cp, calle, numeroint, numeroext) values('dulcemaria', 'jimenez', 'camacho', '2003-05-10', 'F', '5535095067', 'dulcemaria@yahoo.com', 'chalco', '1234', '66589', 'villas','15', '78');
+insert into alumno(nombre, apat, amat, fechadenacimiento, sexo, celular, email,localidad, contraseña, cp, calle, numeroint, numeroext) values('carlos', 'contreras', 'romero', '1998-05-21', 'M', '552903945', 'carlosromero@gmailcom', 'lapaz', '1234', '56589', 'jardines','10', '38');
+insert into alumno(nombre, apat, amat, fechadenacimiento, sexo, celular, email,localidad, contraseña, cp, calle, numeroint, numeroext) values('fatima', 'reyes', 'paz', '1997-09-20', 'F', '55389345', 'fatimamichellreyes@gmailcom', 'iztaplapa', '1234', '09239', 'zonapellon','12', '24');
+insert into alumno(nombre, apat, amat, fechadenacimiento, sexo, celular, email,localidad, contraseña, cp, calle, numeroint, numeroext) values('maria', 'zuñiga', 'mejia', '1996-05-20', 'F', '55673846', 'mariaevelia23@gmailcom', 'chalco', '2344', '9239', 'villa','12', '4');
+
+
+
+
